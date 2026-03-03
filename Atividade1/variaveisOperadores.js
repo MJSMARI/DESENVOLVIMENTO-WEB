9806a90e92779529439757deecf9319bb4fcb383
Marilinda <mariliadejesussilv@gmail.com>
2026-03-02 21:10:11 -0300

    commit atividade1

---
+++ Atividade1/variaveisOperadores.js
@@ -0,0 +1,20 @@
+var mais1 = 58;
+var mais2 = 65;
+var resultadomaismais = mais1 + mais2;
+console.log(`Resultado da soma entre ${mais1} e ${mais2} é igual a ${resultadomaismais}`);
+
+var menos1 = 67;
+var menos2 = 34;
+var resultadomenosmenos = menos1 - menos2;
+console.log(`Resultado da subtração entre ${menos1} e ${menos2} é igual a ${resultadomenosmenos}`);
+
+var mult1 = 563;
+var mult2 = 620;
+var resultadomultmult = mult1 * mult2;
+console.log(`Resultado da multiplicação entre ${mult1} e ${mult2} é igual a ${resultadomultmult}`);
+
+var div1 = 986;
+var div2 = 52;
+var resultadodivdiv = div1 / div2;
+console.log(`Resultado da divisão entre ${div1} e ${div2} é igual a ${resultadodivdiv}`);
+
