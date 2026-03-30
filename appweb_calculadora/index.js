01bb6a7ff44b2de340f3ba80cb7e88638d21c4fe
mari <mariliadejesusilv@gmail.com>
2026-03-30 20:38:10 -0300

    atualizacao.30.03

--- appweb_calculadora/index.js
+++ appweb_calculadora/index.js
@@ -1,5 +1,10 @@
+
+//para acessar: C:\Users\maril\DESENVOLVIMENTO-WEB\appweb_calculadora>node .\index.js
+
 //CARREGA FRAMEWORK EXPRESS
 const express = require('express');
+
+const calculadora = require('./calculadora');
 
 //CRIA UM APP USANDO A FUNÇÃO EXPRESS()
 const app = express();
@@ -15,6 +20,17 @@ app.get('/bem-vindes', (req, res)=>{
     res.send("<h1>Lucas GAYzao!</h1> <p>VAI WILSON<p>");
 });
 
+app.get('/OLa/:nome', (req, res)=>{
+    res.send(`ola, ${req.params.nome}`);
+});  
+
+app.get('/calculadora//somar/:a/:b', (req, res)=>{
+    let a = parseInt(req.params.a);
+    let b = parseInt(req.params.b);
+    let resultado = calculadora.somar(a, b);
+    res.send(`${a} + ${b} = ${resultado}`);
+});  
+ 
 //FORMA DE CRIAR UMA FUNÇÃO, SEM DEFINIR ELA.
 app.listen(PORT, ()=>{
     console.log('app rodando na porta ' + PORT);
